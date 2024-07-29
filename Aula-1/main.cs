216442728ccdb39bfcef4ec867d4f5875a3f68be
Marcos  Souza <35493688-Kainan-Chiabai@users.noreply.replit.com>
2024-07-29 12:15:27 +0000

    Initial commit

---
+++ Aula-1/main.cs
@@ -0,0 +1,8 @@
+using System;// entrada para uso
+
+class Aula1{ // main do codigo
+  public static void Main(string[] args){ // metodo principal
+  int v = 0; // variavel inteira
+   Console.WriteLine (v);// imprime na tela a variavel v 
+  }
+}
